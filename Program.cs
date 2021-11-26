48768893ae84fedac0f8ea760bbf65b736607a55
Tobias Tengler <ttengler@artiso.com>
2021-11-26 20:06:44 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using GreenDonut;
+using HotChocolate;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.Run();
+
+public class Query
+{
+    public List<Wrapper> GetWrappers() => new List<Wrapper>
+    {
+        new Wrapper { Id = 1 },
+        new Wrapper { Id = 2 },
+        new Wrapper { Id = 3 }
+    };
+}
+
+public class Wrapper
+{
+    public int Id { get; set; }
+
+    public Task<string> GetValueAsync([Parent] Wrapper wrapper, [DataLoader] ValueDataLoader dataLoader)
+    {
+        return dataLoader.LoadAsync(wrapper.Id);
+    }
+}
+
+public class ValueDataLoader : BatchDataLoader<int, string>
+{
+    public ValueDataLoader(IBatchScheduler batchScheduler, DataLoaderOptions? options = null)
+        : base(batchScheduler, options)
+    {
+    }
+
+    protected override async Task<IReadOnlyDictionary<int, string>> LoadBatchAsync(IReadOnlyList<int> keys, CancellationToken cancellationToken)
+    {
+        await Task.Delay(2000);
+
+        return keys.ToDictionary(k => k, k => $"MyValue:{k}");
+    }
+}
